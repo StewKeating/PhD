06e5a09f0fe5a61a2600350b6319d089f6719c14
StewKeating <95417415+StewKeating@users.noreply.github.com>
2023-05-16 12:12:37 +0100

    Update Trait_rating_fix.py

--- Trait_rating_fix.py
+++ Trait_rating_fix.py
@@ -15,60 +15,16 @@ import tkinter as tk
 
 g = pd.read_excel('PhD_Study1_V8.xlsx','sheet1')
 d = g.columns.tolist()
-print(d[28])
-'''
-print(g.iloc[0,26])# all characteristics from row
-print(g.iloc[0,28])#Competence
-print(g.iloc[0,29])#Integrity
-print(g.iloc[0,30])#Trustworthiness
-print(g.iloc[0,31])#Charisma
-print(g.iloc[0,32])#Empathy
-print(g.iloc[0,33])#Warmth
-print(g.iloc[0,34])#Expertise
-print(g.iloc[0,35])#Similarity
-print(g.iloc[0,36])#Familiarity
-print(g.iloc[0,37])#Likeability
-print(g.iloc[0,38])#Attractiveness
-
-print(g.iloc[0,39])#self character 1
-print(g.iloc[0,40])#self rating 1
-print(g.iloc[0,41])#self character 2
-print(g.iloc[0,42])#self rating 2
-print(g.iloc[0,43])#self character 3
-print(g.iloc[0,44])#self rating 3
-print(g.iloc[0,45])#self character 4
-print(g.iloc[0,46])#self rating 4
-print(g.iloc[0,47])#self character 5
-print(g.iloc[0,48])#self rating 5
-print(g.iloc[0,49])#self character 6
-print(g.iloc[0,50])#self rating 6
-print(g.iloc[0,51])#self character 7
-print(g.iloc[0,52])#self rating 7
-print(g.iloc[0,53])#self character 8
-print(g.iloc[0,54])#self rating 8
-print(g.iloc[0,55])#self character 9
-print(g.iloc[0,56])#self rating 9
-print(g.iloc[0,57])#self character 10
-print(g.iloc[0,58])#self rating 10
-print(g.iloc[0,59])#self character 11
-print(g.iloc[0,60])#self rating 11
-print(g.iloc[0,61])#self character 12
-print(g.iloc[0,62])#self rating 12
-print(g.iloc[0,63])#self character 13
-print(g.iloc[0,64])#self rating 13
-'''
-
-
 
 class PopUp():
     def __init__(self, parent):
         super().__init__()
         self.num = 0
-        self.label0 = tk.Label(parent, text=str(g.iloc[self.num,26]),width=100,height=20).grid(row=0,column=0,rowspan=24)# list of all characteristics
+        self.label0 = self.rating_box(parent,self.num,26,0,0, 100, 20,24)# list of all characteristics
 #researcher defined labels     
         self.label1a = tk.Label(parent, text=str(d[28]),width=15,height=2).grid(row=0,column=2,rowspan=1)#comp
         self.label1a = tk.Label(parent, text=str(d[29]),width=15,height=2).grid(row=1,column=2,rowspan=1)#int
-        self.label1a = tk.Label(parent, text=str(d[30]),width=15,height=2).grid(row=2,column=2,rowspan=1) #trust
+        self.label1a = tk.Label(parent, text=str(d[30]),width=15,height=2).grid(row=2,column=2,rowspan=1)#trust
         self.label1a = tk.Label(parent, text=str(d[31]),width=15,height=2).grid(row=3,column=2,rowspan=1)#charisma
         self.label1a = tk.Label(parent, text=str(d[32]),width=15,height=2).grid(row=4,column=2,rowspan=1)#empathy
         self.label1a = tk.Label(parent, text=str(d[33]),width=15,height=2).grid(row=5,column=2,rowspan=1)#warmth
@@ -171,120 +127,152 @@ class PopUp():
         self.RowSkip.grid(row=14, column=3)
         SkpBtn = tk.Button(parent,text='RowSkip',command=self.SkpPress).grid(row=14, column=4)
 
-    def rating_box(self,parent, excel_row_no,excel_column_no, gui_row_no, gui_col_no, this_width=5):
-        self.label1 = tk.Text(parent, width=this_width, height=1)
-        self.label1.insert(0.0,str(g.iloc[excel_row_no,excel_column_no]))
-        self.label1.grid(row=gui_row_no,column=gui_col_no,rowspan=1)
-        return (self.label1)
+    def rating_box(self,parent, excel_row_no,excel_column_no, gui_row_no, gui_col_no, this_width=5, this_height=1, this_rowspan=1):
+        self.label = tk.Text(parent, width=this_width, height=this_height)
+        self.label.insert(0.0,str(g.iloc[excel_row_no,excel_column_no]))
+        self.label.grid(row=gui_row_no,column=gui_col_no,rowspan=this_rowspan)
+        return (self.label)
 
+    def clear_and_replace(self, row_num):
+        self.label0.delete(0.0,'end')
+        self.label1.delete(0.0,'end')
+        self.label2.delete(0.0,'end')
+        self.label3.delete(0.0,'end')
+        self.label4.delete(0.0,'end')
+        self.label5.delete(0.0,'end')
+        self.label6.delete(0.0,'end')
+        self.label7.delete(0.0,'end')
+        self.label8.delete(0.0,'end')
+        self.label9.delete(0.0,'end')
+        self.label10.delete(0.0,'end')
 
+        self.label11.delete(0.0,'end')
+        self.label13.delete(0.0,'end')
+        self.label14.delete(0.0,'end')
+        self.label15.delete(0.0,'end')
+        self.label16.delete(0.0,'end')
+        self.label17.delete(0.0,'end')
+        self.label18.delete(0.0,'end')
+        self.label19.delete(0.0,'end')
+        self.label20.delete(0.0,'end')
+
+        self.label21.delete(0.0,'end')
+        self.label22.delete(0.0,'end')
+        self.label23.delete(0.0,'end')
+        self.label24.delete(0.0,'end')
+        self.label25.delete(0.0,'end')
+        self.label26.delete(0.0,'end')
+        self.label27.delete(0.0,'end')
+        self.label28.delete(0.0,'end')
+        self.label29.delete(0.0,'end')
+        self.label30.delete(0.0,'end')
+
+        self.label31.delete(0.0,'end')
+        self.label32.delete(0.0,'end')
+        self.label33.delete(0.0,'end')
+        self.label34.delete(0.0,'end')
+        self.label35.delete(0.0,'end')
+        self.label36.delete(0.0,'end')
+        self.label37.delete(0.0,'end')
+        self.label38.delete(0.0,'end')
+
+        self.label0.insert(0.0,f'{g.iloc[self.num,26]}')
+        self.label1.insert(0.0,f'{g.iloc[self.num,28]}')
+        self.label2.insert(0.0,f'{g.iloc[self.num,29]}')
+        self.label3.insert(0.0,f'{g.iloc[self.num,30]}')
+        self.label4.insert(0.0,f'{g.iloc[self.num,31]}')
+        self.label5.insert(0.0,f'{g.iloc[self.num,32]}')
+        self.label6.insert(0.0,f'{g.iloc[self.num,33]}')
+        self.label7.insert(0.0,f'{g.iloc[self.num,34]}')
+        self.label8.insert(0.0,f'{g.iloc[self.num,35]}')
+        self.label9.insert(0.0,f'{g.iloc[self.num,36]}')
+        self.label10.insert(0.0,f'{g.iloc[self.num,37]}')
+        self.label11.insert(0.0,f'{g.iloc[self.num,38]}')
+
+        self.label13.insert(0.0,f'{g.iloc[self.num,39]}')
+        self.label14.insert(0.0,f'{g.iloc[self.num,40]}')
+        self.label15.insert(0.0,f'{g.iloc[self.num,41]}')
+        self.label16.insert(0.0,f'{g.iloc[self.num,42]}')
+        self.label17.insert(0.0,f'{g.iloc[self.num,43]}')
+        self.label18.insert(0.0,f'{g.iloc[self.num,44]}')
+        self.label19.insert(0.0,f'{g.iloc[self.num,45]}')
+        self.label20.insert(0.0,f'{g.iloc[self.num,46]}')
+        self.label21.insert(0.0,f'{g.iloc[self.num,47]}')
+        self.label22.insert(0.0,f'{g.iloc[self.num,48]}')
+        self.label23.insert(0.0,f'{g.iloc[self.num,49]}')
+        self.label24.insert(0.0,f'{g.iloc[self.num,50]}')
+
+        self.label25.insert(0.0,f'{g.iloc[self.num,51]}')
+        self.label26.insert(0.0,f'{g.iloc[self.num,52]}')
+        self.label27.insert(0.0,f'{g.iloc[self.num,53]}')
+        self.label28.insert(0.0,f'{g.iloc[self.num,54]}')
+        self.label29.insert(0.0,f'{g.iloc[self.num,55]}')
+        self.label30.insert(0.0,f'{g.iloc[self.num,56]}')
+        self.label31.insert(0.0,f'{g.iloc[self.num,57]}')
+        self.label32.insert(0.0,f'{g.iloc[self.num,58]}')
+        self.label33.insert(0.0,f'{g.iloc[self.num,59]}')
+        self.label34.insert(0.0,f'{g.iloc[self.num,60]}')
+        self.label35.insert(0.0,f'{g.iloc[self.num,61]}')
+        self.label36.insert(0.0,f'{g.iloc[self.num,62]}')
+        self.label37.insert(0.0,f'{g.iloc[self.num,63]}')
+        self.label38.insert(0.0,f'{g.iloc[self.num,64]}')
+    
     #function to skip to a certain row
     def SkpPress(self):
-        print('beans')
-            
-        #print(f'{self.num}')
         Skip_to_this_row = self.RowSkip.get()
         print(Skip_to_this_row)
-        #print(type(x))
         self.num = int(self.RowSkip.get())
-        #print(f'{self.num}')
-        ##self.num = int(self.RowSkip.get())
-        #self.LabelNum["text"] = f'Row number = {self.num}'
-
-
-        #self.label0.delete(0.0,'end')
-        #self.Label2.delete(0.0,'end')
-        #self.Label3.delete(0.0,'end')
-        #self.Label4.delete(0.0,'end')
-
-        #self.MT1.delete(0,'end')
-        #self.MT1n.delete(0,'end')
-        #self.MT2.delete(0,'end')
-        #self.MT2n.delete(0,'end')
-        #self.MT3.delete(0,'end')
-        #self.MT3n.delete(0,'end')
-        #self.MT4.delete(0,'end')
-        #self.MT4n.delete(0,'end')
-        #self.MT5.delete(0,'end')
-        #self.MT5n.delete(0,'end')
-
-        self.label0["text"] = f'{g.iloc[self.num,26]}'
-        #self.label0.insert(0.0,f'{g.iloc[self.num,26]}')
-        #self.Label2.insert(0.0,f'{MainDF.iloc[self.num,25]}')
-        #self.Label3.insert(0.0,f'{MainDF.iloc[self.num,26]}')
-        #self.Label4.insert(0.0,f'{MainDF.iloc[self.num,27]}')
-
-        #self.MT1.insert(0,f'{MainDF.iloc[self.num,45]}')
-        #self.MT1n.insert(0,f'{MainDF.iloc[self.num,46]}')
-        #self.MT2.insert(0,f'{MainDF.iloc[self.num,47]}')
-        #self.MT2n.insert(0,f'{MainDF.iloc[self.num,48]}')
-        #self.MT3.insert(0,f'{MainDF.iloc[self.num,49]}')
-        #self.MT3n.insert(0,f'{MainDF.iloc[self.num,50]}')
-        #self.MT4.insert(0,f'{MainDF.iloc[self.num,51]}')
-        #self.MT4n.insert(0,f'{MainDF.iloc[self.num,52]}')
-        #self.MT5.insert(0,f'{MainDF.iloc[self.num,53]}')
-        #self.MT5n.insert(0,f'{MainDF.iloc[self.num,54]}'
+        self.clear_and_replace(self.num)
         
-    #function to save the box content/ move ahead 1 row
-    #def save_and_move(self,parent):
+    #save the boxes and move to the next row
     def BtnPress(self):
-        print('eggs')
-        #MainDF.iloc[self.num,24] = self.Label1.get(0.0,'end')
-        #MainDF.iloc[self.num,25] = self.Label2.get(0.0,'end')
-        #MainDF.iloc[self.num,26] = self.Label3.get(0.0,'end')
-        #MainDF.iloc[self.num,27] = self.Label4.get(0.0,'end')
+        g.iloc[self.num,26] = self.label0.get(0.0,'end')
+        g.iloc[self.num,28] = self.label1.get(0.0,'end')
+        g.iloc[self.num,29] = self.label2.get(0.0,'end')
+        g.iloc[self.num,30] = self.label3.get(0.0,'end')
+        g.iloc[self.num,31] = self.label4.get(0.0,'end')
+        g.iloc[self.num,32] = self.label5.get(0.0,'end')
+        g.iloc[self.num,33] = self.label6.get(0.0,'end')
+        g.iloc[self.num,34] = self.label7.get(0.0,'end')
+        g.iloc[self.num,35] = self.label8.get(0.0,'end')
+        g.iloc[self.num,36] = self.label9.get(0.0,'end')
+        g.iloc[self.num,37] = self.label10.get(0.0,'end')
+        g.iloc[self.num,38] = self.label11.get(0.0,'end')
 
-        #MainDF.iloc[self.num,45] = self.MT1.get()
-        #MainDF.iloc[self.num,46] = self.MT1n.get()
-        #MainDF.iloc[self.num,47] = self.MT2.get()
-        #MainDF.iloc[self.num,48] = self.MT2n.get()
-        #MainDF.iloc[self.num,49] = self.MT3.get()
-        #MainDF.iloc[self.num,50] = self.MT3n.get()
-        #MainDF.iloc[self.num,51] = self.MT4.get()
-        #MainDF.iloc[self.num,52] = self.MT4n.get()
-        #MainDF.iloc[self.num,53] = self.MT5.get()
-        #MainDF.iloc[self.num,54] = self.MT5n.get()
-        ##MainDF.iloc[self.num,56] = self.Attractiveness.get()
-        #MainDF.to_excel("PhD_Study1_V7.xlsx",sheet_name="sheet1",index=False)
-        #self.num+=1
+        g.iloc[self.num,39] = self.label13.get(0.0,'end')
+        g.iloc[self.num,40] = self.label14.get(0.0,'end')
+        g.iloc[self.num,41] = self.label15.get(0.0,'end')
+        g.iloc[self.num,42] = self.label16.get(0.0,'end')
+        g.iloc[self.num,43] = self.label17.get(0.0,'end')
+        g.iloc[self.num,44] = self.label18.get(0.0,'end')
+        g.iloc[self.num,45] = self.label19.get(0.0,'end')
+        g.iloc[self.num,46] = self.label20.get(0.0,'end')
+        g.iloc[self.num,47] = self.label21.get(0.0,'end')
+        g.iloc[self.num,48] = self.label22.get(0.0,'end')
+        g.iloc[self.num,49] = self.label23.get(0.0,'end')
+        g.iloc[self.num,50] = self.label24.get(0.0,'end')
 
-        #self.Label1.delete(0.0,'end')
-        #self.Label2.delete(0.0,'end')
-        #self.Label3.delete(0.0,'end')
-        #self.Label4.delete(0.0,'end')
+        g.iloc[self.num,51] = self.label25.get(0.0,'end')
+        g.iloc[self.num,52] = self.label26.get(0.0,'end')
+        g.iloc[self.num,53] = self.label27.get(0.0,'end')
+        g.iloc[self.num,54] = self.label28.get(0.0,'end')
+        g.iloc[self.num,55] = self.label29.get(0.0,'end')
+        g.iloc[self.num,56] = self.label30.get(0.0,'end')
+        g.iloc[self.num,57] = self.label31.get(0.0,'end')
+        g.iloc[self.num,58] = self.label32.get(0.0,'end')
+        g.iloc[self.num,59] = self.label33.get(0.0,'end')
+        g.iloc[self.num,60] = self.label34.get(0.0,'end')
+        g.iloc[self.num,61] = self.label35.get(0.0,'end')
+        g.iloc[self.num,62] = self.label36.get(0.0,'end')
+        g.iloc[self.num,63] = self.label37.get(0.0,'end')
+        g.iloc[self.num,64] = self.label38.get(0.0,'end')
 
-        #self.MT1.delete(0,'end')
-        #self.MT1n.delete(0,'end')
-        #self.MT2.delete(0,'end')
-        #self.MT2n.delete(0,'end')
-        #self.MT3.delete(0,'end')
-        #self.MT3n.delete(0,'end')
-        #self.MT4.delete(0,'end')
-        #self.MT4n.delete(0,'end')
-        #self.MT5.delete(0,'end')
-        #self.MT5n.delete(0,'end')
+        g.to_excel("PhD_Study1_V8.xlsx",sheet_name="sheet1",index=False)
         
-        #self.Label1.insert(0.0,f'{MainDF.iloc[self.num,24]}')
-        #self.Label2.insert(0.0,f'{MainDF.iloc[self.num,25]}')
-        #self.Label3.insert(0.0,f'{MainDF.iloc[self.num,26]}')
-        #self.Label4.insert(0.0,f'{MainDF.iloc[self.num,27]}')
-
-        #self.MT1.insert(0,f'{MainDF.iloc[self.num,45]}')
-        #self.MT1n.insert(0,f'{MainDF.iloc[self.num,46]}')
-        #self.MT2.insert(0,f'{MainDF.iloc[self.num,47]}')
-        #self.MT2n.insert(0,f'{MainDF.iloc[self.num,48]}')
-        #self.MT3.insert(0,f'{MainDF.iloc[self.num,49]}')
-        #self.MT3n.insert(0,f'{MainDF.iloc[self.num,50]}')
-        #self.MT4.insert(0,f'{MainDF.iloc[self.num,51]}')
-        #self.MT4n.insert(0,f'{MainDF.iloc[self.num,52]}')
-        #self.MT5.insert(0,f'{MainDF.iloc[self.num,53]}')
-        #self.MT5n.insert(0,f'{MainDF.iloc[self.num,54]}')
-
-
+        self.num += 1
+        self.clear_and_replace(self.num) 
 
 root = tk.Tk()
 PopUp(root)
-root.mainloop()
-
-        
+root.mainloop()        
         
